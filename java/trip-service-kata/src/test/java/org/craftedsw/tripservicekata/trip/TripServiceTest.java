f42774ce835c298f6951613058ac110ae5a80478
Habiba Kessraoui <habiba.kessraoui@sofrecom.com>
2021-12-01 10:16:44 +0100

    Working...

--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -1,5 +1,13 @@
 package org.craftedsw.tripservicekata.trip;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
 public class TripServiceTest {
 	
+	@Test
+	public void testName() throws Exception {
+		
+	}
 }
