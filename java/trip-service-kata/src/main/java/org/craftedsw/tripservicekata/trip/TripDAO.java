30dec59b3ff9bc28ddc0ae1169f4299011903a0b
johan.martinsson <johan.martinsson@dexyos.fr>
2021-12-01 12:39:25 +0100

    Working...

--- java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripDAO.java
+++ java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripDAO.java
@@ -7,12 +7,15 @@ import org.craftedsw.tripservicekata.user.User;
 
 public class TripDAO {
 
+    /**
+     * @deprecated please use instance method
+     */
 	public static List<Trip> findTripsByUser(User user) {
 		throw new CollaboratorCallException(
 				"TripDAO should not be invoked on an unit test.");
 	}
 
-    protected List<Trip> getUserTripList(User user) {
+    public List<Trip> getUserTripList(User user) {
         return TripDAO.findTripsByUser(user);
     }
 
